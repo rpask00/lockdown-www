8c725184c573918f7153ceff058649eeea608ac8
Rafal Pasko <rp.261000@gmail.com>
2023-09-11 15:59:07 +0200

    App init.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'lockdown-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lockdown-www';
+  readonly window: Window = window;
 }
